7c068a660b249f045f41cac1f69e4fe037753db3
QING的台式机\10554 <1055428094@qq.com>
2024-07-14 23:27:52 +0800

    feat:添加全景图和水面

--- src/components/threeStage/index.vue
+++ src/components/threeStage/index.vue
@@ -5,6 +5,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import * as THREE from "three";
+import { Water } from "three/examples/jsm/objects/Water2";
 function loadSceneModel(scene) {
   console.log("loadSceneModel");
 
@@ -20,18 +21,65 @@ function loadSceneModel(scene) {
   gltfLoader.setDRACOLoader(dracoLoader);
   gltfLoader.load("./model/scene.glb", (gltf) => {
     console.log(gltf);
+    const model = gltf.scene;
+    model.traverse((child) => {
+      if (child.name === "Plane") {
+        child.visible = false;
+      }
+    });
     scene.add(gltf.scene);
   });
 }
 
+// 加载全景图
+function loadPanorama(scene) {
+  const rgbLoader = new RGBELoader();
+  rgbLoader.load("./textures/sky.hdr", (envMap) => {
+    envMap.mapping = THREE.EquirectangularReflectionMapping;
+    // 设置环境贴图
+    scene.background = envMap;
+    scene.environment = envMap;
+  });
+}
+
+function loadWater(scene) {
+  const waterGeometry = new THREE.PlaneGeometry(1000, 1000);
+  const water = new Water(waterGeometry, {
+    textureWidth: 1024,
+    textureHeight: 1024,
+    waterNormals: new THREE.TextureLoader().load(
+      "./textures/water/Water_1_M_Normal.jpg",
+      (texture) => {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      }
+    ),
+    alpha: 1,
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distorionScale: 3.7,
+    fog: scene.fog !== undefined,
+  });
+  water.rotation.x = -Math.PI / 2;
+  water.position.y = -0.4;
+  scene.add(water);
+}
+
 onMounted(() => {
   const { scene, camera, renderer } = initThreeStage();
+  // 设置色调映射
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  scene.toneMapping = THREE.ACESFilmicToneMapping;
+  scene.toneMappingExposure = 1;
   loadSceneModel(scene);
   camera.position.set(30, 30, 30);
   // 创建平行光
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
   directionalLight.position.set(10, 10, 10);
   scene.add(directionalLight);
+  loadPanorama(scene);
+  // 加载水面
+  loadWater(scene);
 });
 </script>
 
